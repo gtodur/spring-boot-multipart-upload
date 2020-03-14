cdc7fd9fd53053b3aef471e7b91415acbe600db3
gtodur <gtodur@outlook.com>
2020-03-14 10:07:02 +0530

    First commit

---
+++ src/main/java/com/df/springboot/multipart/upload/SpringBootMultipartUploadApplication.java
@@ -0,0 +1,25 @@
+package com.df.springboot.multipart.upload;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class SpringBootMultipartUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultipartUploadApplication.class, args);
+	}
+	
+	@Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
+
+}
---
+++ src/test/java/com/df/springboot/multipart/upload/SpringBootMultipartUploadApplicationTests.java
@@ -0,0 +1,16 @@
+package com.df.springboot.multipart.upload;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMultipartUploadApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
